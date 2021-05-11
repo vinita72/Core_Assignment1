61dcea3d5b13a8aed111498159bdb701314842fb
vinita <imvinitarajoria@gmail.com>
2021-05-11 19:03:03 +0530

    Assignment1

---
+++ injectionSetter/src/main/java/com/injection/injectionSetter/Customer.java
@@ -0,0 +1,42 @@
+package com.injection.injectionSetter;
+
+
+public class Customer {
+	private int id;  
+	private String name;  
+	private Address address;  
+	  
+	 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	 void displayInfo() {
+		 System.out.println("Hello");
+		System.out.println(id+" "+name);  
+	    System.out.println(address);  		
+	}
+	
+	}  
+
+
---
+++ injectionSetter/src/test/java/com/injection/injectionSetter/InjectionSetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.injection.injectionSetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionSetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
